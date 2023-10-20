bc9edef4696635e3a177608a1c66c8651f92509c
DeroMal <derrickmal123@gmail.com>
2023-10-20 11:41:37 +0300

    Further modifications to utilize the client side sdk other than the server side

--- public/admin.js
+++ public/admin.js
@@ -1,58 +1,68 @@
-import './logout.js';
+import './logout.js'
 
 // Global variables to store all users and manage pagination
-let allUsers = [];
-let currentPage = 1;
-const pageSize = 10;
+let allUsers = []
+let currentPage = 1
+const pageSize = 10
 
 // DOM Elements
-const prevPageButton = document.getElementById('prevPage');
-const nextPageButton = document.getElementById('nextPage');
-const userList = document.getElementById('user-list');
-const spinner = document.getElementById('spinner');
+const prevPageButton = document.getElementById('prevPage')
+const nextPageButton = document.getElementById('nextPage')
+const userList = document.getElementById('user-list')
+const spinner = document.getElementById('spinner')
+const profileButton = document.getElementById('profileButton')
+const homeButton = document.getElementById('homeButton')
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', onDocumentLoaded);
-prevPageButton.addEventListener('click', prevPage);
-nextPageButton.addEventListener('click', nextPage);
+document.addEventListener('DOMContentLoaded', onDocumentLoaded)
+prevPageButton.addEventListener('click', prevPage)
+nextPageButton.addEventListener('click', nextPage)
+if (profileButton)
+  profileButton.addEventListener('click', () => navigateTo('profile.html'))
+if (homeButton)
+  homeButton.addEventListener('click', () => navigateTo('home.html'))
 
-// Functions
-function onDocumentLoaded() {
-  populateUserInfo();
-  loadUsers();
+// Function Definitions
+function onDocumentLoaded () {
+  populateUserInfo()
+  loadUsers()
+  setupAdditionalListeners()
 }
 
-async function loadUsers() {
-  spinner.style.display = 'block'; // Show spinner
-  await fetchAllUsers();
-  renderUsers();
-  spinner.style.display = 'none'; // Hide spinner
+function setupAdditionalListeners () {
+  document
+    .getElementById('delete-button')
+    .addEventListener('click', deleteSelectedUsers)
+  document
+    .getElementById('fetch-button')
+    .addEventListener('click', fetchAllUsers)
 }
 
-async function fetchAllUsers() {
-  try {
-    const response = await fetch('/users');
-    const data = await response.json();
-    allUsers = data.users;
-  } catch (error) {
-    console.error('Error fetching all users:', error);
+async function loadUsers () {
+  toggleSpinner(true)
+  await fetchAllUsers()
+  renderUsers()
+  toggleSpinner(false)
+}
+
+function renderUsers () {
+  if (!allUsers) {
+    console.error('No users data available')
+    return
   }
+  const startIndex = (currentPage - 1) * pageSize
+  const endIndex = currentPage * pageSize
+  const usersToRender = allUsers.slice(startIndex, endIndex)
+
+  userList.innerHTML = '' // Clear previous user list
+  usersToRender.forEach(renderUserRow)
+
+  updatePaginationButtons()
 }
 
-function renderUsers() {
-  const startIndex = (currentPage - 1) * pageSize;
-  const endIndex = currentPage * pageSize;
-  const usersToRender = allUsers.slice(startIndex, endIndex);
-
-  userList.innerHTML = ''; // Clear previous user list
-  usersToRender.forEach(renderUserRow);
-
-  updatePaginationButtons();
-}
-
-function renderUserRow(user) {
-  const labels = Array.isArray(user.labels) ? user.labels.join(', ') : 'N/A';
-  const userRow = document.createElement('tr');
+function renderUserRow (user) {
+  const labels = Array.isArray(user.labels) ? user.labels.join(', ') : 'N/A'
+  const userRow = document.createElement('tr')
   userRow.innerHTML = `
     <td><input type="checkbox" value="${user.$id}" /></td>
     <td>${user.name}</td>
@@ -61,76 +71,78 @@ function renderUserRow(user) {
     <td>${labels}</td>
     <td>${user.emailVerification}</td>
     <td>${user.phoneVerification}</td>
-  `;
-  userList.appendChild(userRow);
+  `
+  userList.appendChild(userRow)
 }
 
-function updatePaginationButtons() {
-  prevPageButton.disabled = currentPage === 1;
-  nextPageButton.disabled = currentPage * pageSize >= allUsers.length;
+function updatePaginationButtons () {
+  prevPageButton.disabled = currentPage === 1
+  nextPageButton.disabled = currentPage * pageSize >= allUsers.length
 }
 
-function nextPage() {
+function nextPage () {
   if (currentPage * pageSize < allUsers.length) {
-    currentPage += 1;
-    renderUsers();
+    currentPage += 1
+    renderUsers()
   }
 }
 
-function prevPage() {
+function prevPage () {
   if (currentPage > 1) {
-    currentPage -= 1;
-    renderUsers();
+    currentPage -= 1
+    renderUsers()
   }
 }
 
-function populateUserInfo() {
-  const user = JSON.parse(localStorage.getItem('userInfo'));
+function populateUserInfo () {
+  const user = JSON.parse(localStorage.getItem('userInfo'))
   if (user) {
-    document.getElementById('username').innerText = user.name;
+    document.getElementById('username').innerText = user.name
   } else {
-    console.error('Failed to fetch user info');
+    console.error('Failed to fetch user info')
   }
 }
 
-/************** SERVER SIDE REQUIRED **************/
+async function fetchAllUsers () {
+  try {
+    const response = await fetch('/users')
+    const data = await response.json()
+    allUsers = data.users
+  } catch (error) {
+    console.error('Error fetching all users:', error)
+  }
+}
+
+function toggleSpinner (toggle) {
+  spinner.style.display = toggle ? 'block' : 'none'
+}
+
+function navigateTo (url) {
+  window.location.href = url
+}
+
 async function deleteSelectedUsers () {
-  document.getElementById('spinner').style.display = 'block' // Show spinner
+  toggleSpinner(true)
   const checkboxes = [
-      ...document.querySelectorAll('input[type="checkbox"]:checked')
+    ...document.querySelectorAll('input[type="checkbox"]:checked')
   ]
   const userIds = checkboxes.map(checkbox => checkbox.value)
+
+  document.getElementById('spinner').style.display = 'block' // Show spinner
   try {
-      const response = await fetch('/delete-user', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ userIds }) // Send array of user IDs
-      })
-      if (!response.ok) {
-          const errorText = await response.text()
-          console.error('Error deleting users:', errorText)
-      }
+    const response = await fetch('/delete-user', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userIds }) // Send array of user IDs
+    })
+    if (!response.ok) {
+      const errorText = await response.text()
+      console.error('Error deleting users:', errorText)
+    }
   } catch (error) {
-      console.error('Error deleting users:', error)
+    console.error('Error deleting users:', error)
   }
   await fetchAllUsers() // Refresh the user list
   renderUsers() // Render the updated user list
   document.getElementById('spinner').style.display = 'none' // Hide spinner
-}
-/********************************************************************************************************/
-
-// Event listener for profile button
-const profileButton = document.getElementById('profileButton')
-if (profileButton) {
-  profileButton.addEventListener('click', () => {
-    window.location.href = 'profile.html'
-  })
-}
-
-// Event listener for home button
-const homeButton = document.getElementById('homeButton')
-if (homeButton) {
-  homeButton.addEventListener('click', () => {
-    window.location.href = 'home.html'
-  })
 }
--- public/sign-login.js
+++ public/sign-login.js
@@ -1,113 +1,107 @@
 import { client, account } from './appwriteConfig.js';
 
-function toggleLoader (loaderId, toggle) {
-  const loader = document.getElementById(loaderId)
-  loader.style.display = toggle ? 'inline-block' : 'none'
+// DOM Elements
+const signupForm = document.getElementById('signupForm');
+const loginForm = document.getElementById('loginForm');
+const signupEmail = document.getElementById('signupEmail');
+const signupPassword = document.getElementById('signupPassword');
+const signupPhone = document.getElementById('signupPhone');
+const signupUsername = document.getElementById('signupUsername');
+const loginEmail = document.getElementById('loginEmail');
+const loginPassword = document.getElementById('loginPassword');
+const signupLoader = document.getElementById('signupLoader');
+const loginLoader = document.getElementById('loginLoader');
+
+// Event Listeners
+signupForm.addEventListener('submit', onSignupSubmit);
+loginForm.addEventListener('submit', onLoginSubmit);
+
+// Function Definitions
+function toggleLoader(loader, toggle) {
+  loader.style.display = toggle ? 'inline-block' : 'none';
 }
 
-document.getElementById('signupForm').addEventListener('submit', async e => {
-  e.preventDefault()
-
-  const email = document.getElementById('signupEmail').value
-  const password = document.getElementById('signupPassword').value
-  const phone = document.getElementById('signupPhone').value
-  const username = document.getElementById('signupUsername').value
-
-  const formData = new URLSearchParams()
-  formData.append('email', email)
-  formData.append('password', password)
-  formData.append('phone', phone)
-  formData.append('username', username)
-
-  try {
-    const response = await fetch('/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: formData
-    })
-    const data = await response.json()
-    if (data.message === 'Signup successful') {
-      $('#signupModal').modal('hide')
-      $('#loginModal').modal('show')
-    } else {
-      alert(JSON.stringify(data))
-    }
-  } catch (error) {
-    alert('Error signing up: ' + error.message)
-  }
-  toggleLoader('signupLoader', false)
-})
-
-// Function to log in
-async function login(email, password) {
-  try {
-    // Clear local storage
-    localStorage.clear();
-
-    let response = await account.createEmailSession(email, password);
-    console.log('Logging in successful');
-    console.log(response); // Success
-    localStorage.setItem('sessionData', JSON.stringify(response));
-    return response; // return the response for further use if needed
-  } catch (error) {
-    console.error(error); // Failure
-    throw error; // re-throw the error for handling in the calling code
-  }
-}
-
-// Function to fetch account details
-async function fetchAccountDetails() {
-  try {
-    console.log('Fetching User Account Details');
-    const response = await account.get();
-    console.log('The User Account Details:');
-    console.log(response); // Success
-    return response; // return the response for further use if needed
-  } catch (error) {
-    console.error('Failed to get the User Account Details: ', error.message);
-    throw error; // re-throw the error for handling in the calling code
-  }
-}
-
-// Event listener for form submission
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
+async function onSignupSubmit(e) {
   e.preventDefault();
 
-  const email = document.getElementById('loginEmail').value;
-  const password = document.getElementById('loginPassword').value;
+  const email = signupEmail.value;
+  const password = signupPassword.value;
+  const phone = signupPhone.value;
+  const username = signupUsername.value;
+
+  toggleLoader(signupLoader, true);
 
   try {
-    await login(email, password); // Login and wait for it to complete
-    const accountDetails = await fetchAccountDetails(); // Fetch account details and wait for it to complete
-    
-    const uID = accountDetails.$id; // Assign the id in the response to the variable uID
-    console.log(uID);
-
-    if (accountDetails.status) {
-      // Save user data to localStorage
-      localStorage.setItem(
-        'userInfo',
-        JSON.stringify({
-          uID: uID,
-          name: accountDetails.name,
-          labels: accountDetails.labels,
-          email: accountDetails.email,
-          phone: accountDetails.phone,
-          emailVerification: accountDetails.emailVerification,
-          phoneVerification: accountDetails.phoneVerification,
-        })
-      );
-
-      alert('Logging in ...');
-      window.location.href = 'home.html';
-    } else {
-      alert(JSON.stringify(accountDetails));
-    }
+    await performSignup(email, password, username);
+    await performLogin(email, password);
+    const accountDetails = await fetchAccountDetails();
+    handleAccountDetails(accountDetails, 'home.html');
   } catch (error) {
     alert('Error: ' + error.message);
   }
 
-  toggleLoader('loginLoader', false);
-});
+  toggleLoader(signupLoader, false);
+}
+
+async function onLoginSubmit(e) {
+  e.preventDefault();
+
+  const email = loginEmail.value;
+  const password = loginPassword.value;
+
+  toggleLoader(loginLoader, true);
+
+  try {
+    await performLogin(email, password);
+    const accountDetails = await fetchAccountDetails();
+    handleAccountDetails(accountDetails, 'home.html');
+  } catch (error) {
+    alert('Error: ' + error.message);
+  }
+
+  toggleLoader(loginLoader, false);
+}
+
+async function performSignup(email, password, name) {
+  localStorage.clear();
+  const response = await account.create('unique()', email, password, name);
+  console.log('Signup is successful:', response);
+  return response;
+}
+
+async function performLogin(email, password) {
+  localStorage.clear();
+  const response = await account.createEmailSession(email, password);
+  console.log('Logging in successful:', response);
+  localStorage.setItem('sessionData', JSON.stringify(response));
+  return response;
+}
+
+async function fetchAccountDetails() {
+  console.log('Fetching User Account Details');
+  const response = await account.get();
+  console.log('The User Account Details:', response);
+  return response;
+}
+
+function handleAccountDetails(accountDetails, redirectURL) {
+  const uID = accountDetails.$id;
+  console.log(uID);
+
+  if (accountDetails.status) {
+    localStorage.setItem('userInfo', JSON.stringify({
+      uID,
+      name: accountDetails.name,
+      labels: accountDetails.labels,
+      email: accountDetails.email,
+      phone: accountDetails.phone,
+      emailVerification: accountDetails.emailVerification,
+      phoneVerification: accountDetails.phoneVerification
+    }));
+
+    alert('Logging in ...');
+    window.location.href = redirectURL;
+  } else {
+    alert(JSON.stringify(accountDetails));
+  }
+}
