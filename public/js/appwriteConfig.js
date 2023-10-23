c51255e7be6490cd0da273754067fa20678ba95b
DeroMal <derrickmal123@gmail.com>
2023-10-23 17:17:44 +0300

    hardcoded the env vars for testing \

--- public/js/appwriteConfig.js
+++ public/js/appwriteConfig.js
@@ -1,13 +1,12 @@
 // appwriteConfig.js
-import { Client, Account} from 'appwrite';
+import { Client, Account } from 'appwrite'
 
 const client = new Client()
-  .setEndpoint(process.env.APPWRITE_ENDPOINT) // Your API Endpoint from env var
-  .setProject(process.env.APPWRITE_PROJECT_ID); // Your project ID from env var
+  .setEndpoint('https://cloud.appwrite.io/v1') // Setting your Appwrite endpoint from env var
+  .setProject('651413f38aee140189c2') // Setting your project ID from env var
+// .setEndpoint(process.env.APPWRITE_ENDPOINT) // Your API Endpoint from env var
+// .setProject(process.env.APPWRITE_PROJECT_ID); // Your project ID from env var
 
-  // .setEndpoint('https://cloud.appwrite.io/v1') // Setting your Appwrite endpoint from env var
-  // .setProject('651413f38aee140189c2') // Setting your project ID from env var
+const account = new Account(client)
 
-const account = new Account(client);
-
-export { client, account };
+export { client, account }
